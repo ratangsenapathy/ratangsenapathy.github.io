939f2117637808267e6da32cd8272e710dd489f9
ratansen <ratangsenapathy@gmail.com>
2017-06-08 09:59:21 -0700

    Basic layout that is responsive

--- portfolio.js
+++ portfolio.js
@@ -3,3 +3,16 @@ function toggleMenu(element)
 	element.classList.toggle('active-hamburger-button');
 	document.getElementsByClassName('sidebar pure-menu')[0].classList.toggle('active-menu');
 }
+
+function transition(tabName)
+{	console.log("Why","why");
+	var  element = document.getElementById(tabName);
+	console.log(element);
+	element.style.left = "0px";
+}
+
+function setMainContentWidth()
+{
+	if(window.innerWidth>768)
+	document.getElementsByClassName("main-content")[0].style.width = "" + (window.innerWidth -150) + "px";
+}
