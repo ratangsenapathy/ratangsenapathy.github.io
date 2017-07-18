8933789535635297f039aef34bf3043e0545fb66
ratansen <ratangsenapathy@gmail.com>
2017-07-18 15:30:35 -0700

    Added experience and photo to each page

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,5 @@
+var curTab ="home";
+var tabList = ["home","education","experience","projects","contact"];
 function toggleMenu(element)
 {
 	element.classList.toggle('active-hamburger-button');
@@ -5,14 +7,44 @@ function toggleMenu(element)
 }
 
 function transition(tabName)
-{	console.log("Why","why");
-	var  element = document.getElementById(tabName);
-	console.log(element);
-	element.style.left = "0px";
+{	
+
+	if(tabList.indexOf(tabName)>tabList.indexOf(curTab))
+	{
+		var tabStart = tabList.indexOf(curTab) + 1;
+		var tabEnd = tabList.indexOf(tabName) + 1;
+		for(var i = tabStart;i<tabEnd;i++)
+		{
+			var  element = document.getElementById(tabList[i]);
+			console.log(element);
+			element.style.left = "0px";
+			curTab = tabName;
+		}
+		
+	}
+	else
+	{
+		var tabStart = tabList.indexOf(curTab);
+		var tabEnd = tabList.indexOf(tabName);
+		for(var i = tabStart;i>tabEnd;i--)
+		{
+			var  element = document.getElementById(tabList[i]);
+			console.log(element);
+			element.style.left = "100%";
+			curTab = tabName;
+		}
+	}
 }
 
 function setMainContentWidth()
 {
 	if(window.innerWidth>768)
-	document.getElementsByClassName("main-content")[0].style.width = "" + (window.innerWidth -150) + "px";
+		{
+			var content = document.getElementsByClassName("main-content")
+			var len = content.length;
+			for(var i=0;i<len;i++)
+			{
+				content[i].style.width = "" + (window.innerWidth -150) + "px";
+			}
+		}
 }
